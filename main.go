0f0ada1b99cb4a6866ec1a35928abe2a5252a4ae
klados <kladgeo@gmail.com>
2026-09-01 18:35:18 +0300

    add skills

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+
+	"github.com/go-mysql-org/go-mysql/server"
 )
 
-// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	l, err := net.Listen("tcp", "0.0.0.0:3306")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("MySQL blackhole server listening on 0.0.0.0:3306")
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Println("accept error:", err)
+			continue
+		}
+
+		go func() {
+			conn, err := server.NewConn(c, "root", "", server.EmptyHandler{})
+			if err != nil {
+				log.Println("connection error:", err)
+				c.Close()
+				return
+			}
+			log.Printf("new connection id=%d user=%s\n", conn.ConnectionID(), conn.GetUser())
+
+			for {
+				if err := conn.HandleCommand(); err != nil {
+					log.Printf("connection id=%d closed: %v\n", conn.ConnectionID(), err)
+					return
+				}
+			}
+		}()
 	}
 }
